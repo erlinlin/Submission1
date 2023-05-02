7c41f17a4dfa38979733fdbfb876cc6e3c36b1f7
Erlin <erlinpuspa.n@gmail.com>
2023-05-02 10:09:04 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profil Erlin Puspa Ningrum</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+	<div class="lit-container">
+  <svg viewBox="0 0 960 300">
+    <symbol id="s-text">
+      <text text-anchor="middle" x="50%" y="80%">MyProfile</text>
+    </symbol>
+
+    <g class = "g-ants">
+      <use xlink:href="#s-text" class="text-copy"></use>
+      <use xlink:href="#s-text" class="text-copy"></use>
+      <use xlink:href="#s-text" class="text-copy"></use>
+      <use xlink:href="#s-text" class="text-copy"></use>
+      <use xlink:href="#s-text" class="text-copy"></use>
+    </g>
+  </svg>
+</div>
+		<div class="img-container">
+			<img src="https://storage.googleapis.com/dicoding-storege-subpro/me.jpg" alt="Foto Profil">
+		</div>
+		<div class="info-container">
+			<h1>Name: Erlin Puspa Ningrum</h1>
+			<p>Email: erlinpuspa.n@gmail.com</p>
+			<p>Phone: 082x-xxxx-xxxx</p>
+			<p>About Me:</p>
+			<h4>Sixth semester student at the Muhammadiyah University of Bandung majoring in Informatics. Graduated from Vocational High School in Bani Ma'sum majoring in Software Engineering and currently studying in the BangkiT program
+See more about me on LinkedIn</h4>
+			<a href="https://www.linkedin.com/in/erlin-puspa-ningrum-b75619221"><img src="https://storage.googleapis.com/dicoding-storege-subpro/newicon.png" alt="LinkedIn"></a>
+		</div>
+	</div>
+</body>
+</html>
